03fdb4305cf4c66b056af826d8c5959c9bd066ef
Fernndo Viton <fcode@viton.org>
2017-02-20 22:24:45 -0800

    Consolodate Skils and skill jsx

--- components/skills.jsx
+++ components/skills.jsx
@@ -1,35 +1,43 @@
 import React from 'react'
 import Button from 'react-bootstrap/lib/Button';
 import Table from 'react-bootstrap/lib/Table'
-import Skill from './skill.jsx'
 import Immutible from 'Immutable'
 import { changeSkillRank } from '../actions/index'
 
 export default class Skills extends React.Component {
-    render() {
-        const { store } = this.context;
-        const skillElements = store.getState().character.skills.map((skill, skillName) => {
-            return <Skill key={skillName} value={{skillName: skillName, crossClass:null, total: 0, ranks:skill.ranks }}/>
-        }).toArray()
+	render() {
+		const { store } = this.context;
+		const skillElements = store.getState().character.skills.map((skill, skillName) => {
+			return skillRowElement(store, skillName, skill)
+		}).toArray()
 
-        return <div>
-            <Table striped bordered condensed hover>
-                <thead>
-                <tr>
-                    <th>Skill Name</th>
-                    <th>Cross Class</th>
-                    <th>Total</th>
-                    <th>Ranks</th>
-                    <th></th>
-                </tr>
-                </thead>
-                <tbody>
-                    {skillElements}
-                </tbody>
-            </Table>
-        </div>
-    }
+		return <div>
+			<Table striped bordered condensed hover>
+				<thead>
+					<tr>
+						<th>Skill Name</th>
+						<th>Total</th>
+						<th>Ranks</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					{skillElements}
+				</tbody>
+			</Table>
+		</div>
+	}
 }
 Skills.contextTypes = {
-  store: React.PropTypes.object
+	store: React.PropTypes.object
 }
+
+const skillRowElement = (store, skillName, skill) => <tr key={skillName}>
+	<td>{skillName}</td>
+	<td>{skill.total}</td>
+	<td>{skill.ranks}</td>
+	<td>
+		<Button bsStyle="info" bsSize="xsmall" onClick={() => store.dispatch(changeSkillRank(skillName, 1))}>+</Button>
+		<Button bsStyle="info" bsSize="xsmall" style={{ marginLeft: 5 }} onClick={() => store.dispatch(changeSkillRank(skillName, -1))}>-</Button>
+	</td>
+</tr>
